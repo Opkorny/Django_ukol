8e1e813dd712a335e17544128100371f0b7f47e5
Opkorny <47443110+Opkorny@users.noreply.github.com>
2021-05-07 00:57:32 +0200

    finito

--- my_products/my_products/urls.py
+++ my_products/my_products/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('', RedirectView.as_view(url='products/')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
